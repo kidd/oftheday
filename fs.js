d94bf6110a8cfcf620575f4019495974c2e0c09b
Raimon Grau <raimonster@gmail.com>
2020-10-30 22:42:18 +0000

    parsed mental models from fs.blog
    wget https://fs.blog/mental-models/ |
      pup 'div.rte > p > strong a json{}' |
      jq '[.[] | {href, text}]' > fs.js

---
+++ fs.js
@@ -0,0 +1,170 @@
+[
+  {
+    "href": "https://fs.blog/2015/11/map-and-territory/",
+    "text": "1. The Map is not the Territory"
+  },
+  {
+    "href": "https://fs.blog/2013/12/mental-model-circle-of-competence/",
+    "text": "2. Circle of Competence"
+  },
+  {
+    "href": "https://fs.blog/2018/04/first-principles/",
+    "text": "3. First Principles Thinking"
+  },
+  {
+    "href": "https://fs.blog/2017/06/thought-experiment-how-einstein-solved-difficult-problems/",
+    "text": "4. Thought Experiment"
+  },
+  {
+    "href": "https://fs.blog/2016/04/second-order-thinking/",
+    "text": "5. Second-Order Thinking"
+  },
+  {
+    "href": "https://fs.blog/2018/05/probabilistic-thinking/",
+    "text": "6. Probabilistic Thinking"
+  },
+  {
+    "href": "https://fs.blog/2018/09/bayess-theorem/",
+    "text": "Bayesian Updating"
+  },
+  {
+    "href": "https://fs.blog/2013/10/inversion/",
+    "text": "7. Inversion"
+  },
+  {
+    "href": "https://fs.blog/2019/10/occams-razor/",
+    "text": "8. Occam’s Razor"
+  },
+  {
+    "href": "https://fs.blog/2017/04/mental-model-hanlons-razor/",
+    "text": "9. Hanlon’s Razor"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2017/05/galilean-relativity-invasion-scotland/",
+    "text": "Relativity"
+  },
+  {
+    "href": "https://fs.blog/2018/03/speed-velocity/",
+    "text": "Velocity"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2017/06/activation-energy/",
+    "text": "Activation Energy"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2012/10/the-red-queen-effect/",
+    "text": "The Red Queen Effect"
+  },
+  {
+    "href": "https://fs.blog/2017/10/bias-incentives-reinforcement/",
+    "text": "Incentives"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2011/10/mental-model-feedback-loops/",
+    "text": "Feedback Loops"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2013/12/margin-of-safety/",
+    "text": "Margin of Safety"
+  },
+  {
+    "href": "https://fs.blog/2016/08/multiplicative-systems/",
+    "text": "Multiplying by Zero"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2015/07/regression-to-the-mean/",
+    "text": "Regression to the Mean"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2009/08/should-tiger-woods-mow-his-own-lawn-the-principles-of-comparative-advantage/",
+    "text": "Comparative Advantage"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2009/07/mental-model-supply-and-demand/",
+    "text": "Supply and Demand"
+  },
+  {
+    "href": "https://fs.blog/2013/11/mr-market/",
+    "text": "Mr. Market"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2017/06/counterinsurgency/",
+    "text": "Counterinsurgency"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2017/06/mutually-assured-destruction/",
+    "text": "Mutually Assured Destruction"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2016/08/mental-model-bias-envy-jealousy/",
+    "text": "Tendency to Feel Envy &amp; Jealousy"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2016/09/mental-model-bias-from-liking-loving/",
+    "text": "Tendency to Distort Due to Liking/Loving"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2016/09/bias-from-disliking-hating/",
+    "text": "Disliking/Hating"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2011/08/mental-model-availability-bias/",
+    "text": "Availability Heuristic"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2012/11/mental-model-bias-from-insensitivity-to-base-rates/",
+    "text": "Failure to Account for Base Rates"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2009/09/mental-model-social-proof/",
+    "text": "Social Proof"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2016/04/narrative-fallacy/",
+    "text": "Narrative Instinct"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2013/05/mental-model-bias-from-insensitivity-to-sample-size/",
+    "text": "Tendency to Overgeneralize from Small Samples"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2016/08/commitment-consistency-bias/",
+    "text": "Commitment &amp; Consistency Bias"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2009/08/what-is-hindsight-bias/",
+    "text": "Hindsight Bias"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2011/09/mental-model-kantian-fairness-tendency/",
+    "text": "Sensitivity to Fairness"
+  },
+  {
+    "href": "https://www.farnamstreetblog.com/2016/08/fundamental-attribution-error/",
+    "text": "Fundamental Attribution Error"
+  },
+  {
+    "href": "https://fs.blog/2019/12/survivorship-bias/",
+    "text": "Survivorship Bias"
+  },
+  {
+    "href": "https://fs.blog/2015/06/do-something-syndrome/",
+    "text": "Tendency to Want to Do Something"
+  },
+  {
+    "href": "https://www.fs.blog/2014/02/peter-cathcart-wason-falsification/",
+    "text": "Falsification"
+  },
+  {
+    "href": "https://www.fs.blog/2017/05/confirmation-bias/",
+    "text": "Confirmation Bias"
+  },
+  {
+    "href": "https://www.fs.blog/membership/",
+    "text": "Members"
+  },
+  {
+    "href": "https://lc.fs.blog/c/mental-models",
+    "text": "Learning Community Forum"
+  }
+]
